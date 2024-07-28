7b1f10ae52c7ec9924b0f53da8f8619ae7893cca
ismnurayy <ismnnuray1@gmail.com>
2024-07-28 19:00:19 +0400

    etrbfgs

---
+++ product.js
@@ -0,0 +1,9 @@
+document.getElementById('prev').addEventListener('click', function() {
+    // Logic for previous review
+    alert('Previous review');
+});
+
+document.getElementById('next').addEventListener('click', function() {
+    // Logic for next review
+    alert('Next review');
+});
